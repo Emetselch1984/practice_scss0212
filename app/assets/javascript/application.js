097637cf9f6dc2f35c95510a77277846ff554252
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-02-17 11:02:38 +0900

    Building the Environment Javascript

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,4 +1,5 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
-//= require_tree .
+//= require cal
+//= require main
---
+++ app/assets/javascript/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+   const so = new Calculation()
+    const result = so.plus(2,6)
+    console.log(result)
+});
+// class Calculation {
+//     constructor() {
+//     }
+//     plus(a,b){
+//         return a + b;
+//     }
+// }
+
